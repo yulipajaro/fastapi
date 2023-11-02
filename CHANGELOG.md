378265cba5c0660afabc0039f89611c8810eabc4
miguepoloc <miguepoloc@gmail.com>
2023-11-01 22:21:59 -0500

    :tada: first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## CHANGELOG
+
+### Version 0.1.0 (2023-11-01)
+
+-   Initial release
---
+++ app/main.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from fastapi import FastAPI
+from starlette.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/example_dates")
+def get_example_dates():
+    return FileResponse('data/example_dates.json')
